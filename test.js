94e14478b7e2fd69891aec0ed42e6a2e328bd7c3
AlreadyBored <shilaums@gmail.com>
2020-09-08 22:52:33 +0300

    fix: remove duplicate test & give correct name to test spec

--- test.js
+++ test.js
@@ -1,3 +1,2 @@
-require('./test/task1.test');
-require('./test/task2.test');
-require('./test/task3.test');
+require('./test/timeFormatter.test');
+
--- test/timeFormatter.test.js
+++ test/timeFormatter.test.js
@@ -18,26 +18,6 @@ describe('Task1', () => {
   //Specific requirements
 
   describe('base requirements', () => {
-    it.optional('Format values below 1 minute', () => {
-
-      simpleValues = [
-        5, 10, 30, 45
-      ];
-
-      // Simple syntax
-
-      expect(formatTime(5)).to.be.equal('5 sec');
-      expect(formatTime(10)).to.be.equal('10 sec');
-      expect(formatTime(30)).to.be.equal('30 sec');
-      expect(formatTime(45)).to.be.equal('45 sec');
-
-      // Alternative variant
-
-      simpleValues.forEach(item => {
-        const itemStringified = `${String(item)} sec`;
-        expect(formatTime(item)).to.be.equal(itemStringified);
-      });
-    });
 
     it.optional('Format values below 1 minute', () => {
 
