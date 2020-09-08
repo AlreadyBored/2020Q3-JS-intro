ae61804e6da724745ba855a3a2b2954f8591c779
AlreadyBored <shilaums@gmail.com>
2020-09-08 22:56:43 +0300

    feat: add final version of solution file & remove unnecessary files

--- src/timeFormatter.js
+++ src/timeFormatter.js
@@ -1,6 +1,7 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function solution3(/* arg1 */) {
+module.exports = function timeFornatter(/* sec */) {
   throw new CustomError('Not implemented');
   // remove line with error and write your code here
 };
+
