41f44edc7bf5aebc45c1fee4c4b8defd3f8f1fb2
AlreadyBored <shilaums@gmail.com>
2020-09-08 22:02:08 +0300

    feat: add 1st basic test & fill solution file with some content

--- test/task1.test.js
+++ test/task1.test.js
@@ -4,22 +4,39 @@ it.optional = require('../extensions/it-optional');
 
 Object.freeze(assert);
 
-const solution1 = require('../src/task1.js');
+const formatTime = require('../src/timeFormatter.js');
 
 describe('Task1', () => {
   // Presence requirement
   describe ('variable presence', () => {
-      it.optional('function solution1 exists', () => {
-        expect(solution1).exist;
-        expect(solution1).to.be.instanceOf(Function);
+      it.optional('function formatTime exists', () => {
+        expect(formatTime).exist;
+        expect(formatTime).to.be.instanceOf(Function);
       });
   });
 
   //Specific requirements
 
   describe('base requirements', () => {
-    it.optional('base requirement1', () => {
+    it.optional('Format values below 1 minute', () => {
 
+      simpleValues = [
+        5, 10, 30, 45
+      ];
+
+      // Simple syntax
+
+      expect(formatTime(5)).to.be.equal('5 sec');
+      expect(formatTime(10)).to.be.equal('10 sec');
+      expect(formatTime(30)).to.be.equal('30 sec');
+      expect(formatTime(45)).to.be.equal('45 sec');
+
+      // Alternative variant
+
+      simpleValues.forEach(item => {
+        const itemStringified = `${String(item)} sec`;
+        expect(formatTime(item)).to.be.equal(itemStringified);
+      });
     });
 
   })
